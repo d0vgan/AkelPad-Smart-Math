0d8bba02996a1d6719cc0a6f546b30ad0fe72e26
d0vgan <dvv81@ukr.net>
2026-05-02 23:45:47 +0300

    updating Compile32.bat and Compile64.bat: output to Plugs and Plugs64, smaller binaries

--- Compile32.bat
+++ Compile32.bat
@@ -1,7 +1,11 @@
 @echo off
 set "FB_HOME=C:\tools\FreeBASIC"
 set "PATH=%PATH%;%FB_HOME%;%FB_HOME%\bin\win32"
-windres -F pe-i386 -c 65001 -I "%FB_HOME%\inc\win\rc" -i SmartMath.rc -o SmartMath_res.o
+set "OUT_DIR=%~dp0AkelFiles\Plugs"
+if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
+windres -F pe-i386 -c 65001 -I "%FB_HOME%\inc\win\rc" -i SmartMath.rc -o "%OUT_DIR%\SmartMath_res.o"
 if errorlevel 1 exit /b 1
-fbc -dll -gen gcc -arch 386 -x "SmartMath32.dll" "SmartMath.bas" "SmartMath_Config.bas" "SmartMath_Format.bas" "SmartMath_CopyNormalize.bas" "SmartMath_About.bas" "SmartMath_Menu.bas" "MathParser.bas" "SmartMath_res.o"
+fbc -dll -gen gcc -arch 386 -x "%OUT_DIR%\SmartMath.dll" -strip -O 2 -Wc -Os "SmartMath.bas" "SmartMath_Config.bas" "SmartMath_Format.bas" "SmartMath_CopyNormalize.bas" "SmartMath_About.bas" "SmartMath_Menu.bas" "MathParser.bas" "%OUT_DIR%\SmartMath_res.o"
+if exist "%OUT_DIR%\*.a" del "%OUT_DIR%\*.a"
+if exist "%OUT_DIR%\*.o" del "%OUT_DIR%\*.o"
 REM pause
