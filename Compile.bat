f7c217d0b77e479fdf32336c322acb525688152d
d0vgan <dvv81@ukr.net>
2026-05-03 20:28:56 +0300

    updating Compile*.bat

--- Compile.bat
+++ Compile.bat
@@ -1,5 +1,21 @@
 @echo off
+
+rem Specify the correct path to FreeBASIC here:
 set "FB_HOME=C:\tools\FreeBASIC"
+
+if not exist "%FB_HOME%" (
+    echo ERROR: FreeBASIC not found in "%FB_HOME%". Please specify the correct path to FreeBASIC in "%~nx0".
+    exit /b 1
+)
+if not exist "%FB_HOME%\bin\win32" (
+    echo ERROR: FreeBASIC bin\win32 not found in "%FB_HOME%\bin\win32"
+    exit /b 1
+)
+if not exist "%FB_HOME%\inc\win\rc" (
+    echo ERROR: FreeBASIC inc\win\rc not found in "%FB_HOME%\inc\win\rc"
+    exit /b 1
+)
+
 set "PATH=%PATH%;%FB_HOME%;%FB_HOME%\bin\win32"
 rem UTF-8 in SmartMath.rc needs code page 65001 or accented letters show as mojibake in the DLL
 windres -F pe-i386 -c 65001 -I "%FB_HOME%\inc\win\rc" -i SmartMath.rc -o SmartMath_res.o
--- Compile32.bat
+++ Compile32.bat
@@ -1,4 +1,27 @@
 @echo off
+
+rem Specify the correct path to FreeBASIC here:
+set "FB_HOME=C:\tools\FreeBASIC"
+
+if not exist "%FB_HOME%" (
+    echo ERROR: FreeBASIC not found in "%FB_HOME%". Please specify the correct path to FreeBASIC in "%~nx0".
+    exit /b 1
+)
+if not exist "%FB_HOME%\bin\win32" (
+    echo ERROR: FreeBASIC bin\win32 not found in "%FB_HOME%\bin\win32"
+    exit /b 1
+)
+if not exist "%FB_HOME%\inc\win\rc" (
+    echo ERROR: FreeBASIC inc\win\rc not found in "%FB_HOME%\inc\win\rc"
+    exit /b 1
+)
+
+set "PATH=%PATH%;%FB_HOME%;%FB_HOME%\bin\win32"
+set "OUT_DIR=%~dp0AkelFiles\Plugs"
+if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
+windres -F pe-i386 -c 65001 -I "%FB_HOME%\inc\win\rc" -i SmartMath.rc -o "%OUT_DIR%\SmartMath_res.o"
+if errorlevel 1 exit /b 1
+
 rem fbc flags (see also Compile64.bat):
 rem   -strip     Passes -s to ld: remove symbol table / relocation info from the PE (smaller file).
 rem   -O 2       FreeBASIC compiler optimization level on its IR before emitting C (-gen gcc).
@@ -6,12 +29,7 @@ rem   -Wc -Os    GCC: optimize for size.
 rem   -Wc -O2    GCC: optimize for speed.
 rem 32-bit DLL can be slightly larger than 64-bit for the same source: more stack spills / different
 rem tuning in libgcc+runtime, x87 vs SSE paths, and x64 sometimes shrinking hot code via extra GPRs.
-set "FB_HOME=C:\tools\FreeBASIC"
-set "PATH=%PATH%;%FB_HOME%;%FB_HOME%\bin\win32"
-set "OUT_DIR=%~dp0AkelFiles\Plugs"
-if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
-windres -F pe-i386 -c 65001 -I "%FB_HOME%\inc\win\rc" -i SmartMath.rc -o "%OUT_DIR%\SmartMath_res.o"
-if errorlevel 1 exit /b 1
+
 fbc -dll -gen gcc -arch 386 -x "%OUT_DIR%\SmartMath.dll" -strip -O 2 -Wc -O2 "SmartMath.bas" "SmartMath_Config.bas" "SmartMath_Format.bas" "SmartMath_CopyNormalize.bas" "SmartMath_About.bas" "SmartMath_Menu.bas" "MathParser.bas" "%OUT_DIR%\SmartMath_res.o"
 if exist "%OUT_DIR%\*.a" del "%OUT_DIR%\*.a"
 if exist "%OUT_DIR%\*.o" del "%OUT_DIR%\*.o"
