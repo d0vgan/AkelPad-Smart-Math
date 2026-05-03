c9f5ee70572dd2bda889a6bbd28fe1ab6e6f1791
d0vgan <dvv81@ukr.net>
2026-05-03 21:13:05 +0300

    simplifying the Compile*.bat files; adding UTF-8 BOM to SmartMath.rc

--- Compile.bat
+++ Compile.bat
@@ -11,14 +11,10 @@ if not exist "%FB_HOME%\bin\win32" (
     echo ERROR: FreeBASIC bin\win32 not found in "%FB_HOME%\bin\win32"
     exit /b 1
 )
-if not exist "%FB_HOME%\inc\win\rc" (
-    echo ERROR: FreeBASIC inc\win\rc not found in "%FB_HOME%\inc\win\rc"
-    exit /b 1
-)
 
 set "PATH=%PATH%;%FB_HOME%;%FB_HOME%\bin\win32"
-rem UTF-8 in SmartMath.rc needs code page 65001 or accented letters show as mojibake in the DLL
-windres -F pe-i386 -c 65001 -I "%FB_HOME%\inc\win\rc" -i SmartMath.rc -o SmartMath_res.o
-if errorlevel 1 exit /b 1
-fbc -dll -gen gcc "SmartMath.bas" "SmartMath_Config.bas" "SmartMath_Format.bas" "SmartMath_CopyNormalize.bas" "SmartMath_About.bas" "SmartMath_Menu.bas" "MathParser.bas" "SmartMath_res.o"
+
+fbc -dll -gen gcc "SmartMath.bas" "SmartMath_Config.bas" "SmartMath_Format.bas" "SmartMath_CopyNormalize.bas" "SmartMath_About.bas" "SmartMath_Menu.bas" "MathParser.bas" "SmartMath.rc"
+if exist "*.a" del "*.a"
+if exist "*.o" del "*.o"
 REM pause
--- Compile64.bat
+++ Compile64.bat
@@ -11,16 +11,10 @@ if not exist "%FB_HOME%\bin\win32" (
     echo ERROR: FreeBASIC bin\win32 not found in "%FB_HOME%\bin\win32"
     exit /b 1
 )
-if not exist "%FB_HOME%\inc\win\rc" (
-    echo ERROR: FreeBASIC inc\win\rc not found in "%FB_HOME%\inc\win\rc"
-    exit /b 1
-)
 
 set "PATH=%PATH%;%FB_HOME%;%FB_HOME%\bin\win32"
 set "OUT_DIR=%~dp0AkelFiles\Plugs64"
 if not exist "%OUT_DIR%" mkdir "%OUT_DIR%"
-windres -F pe-x86-64 -c 65001 -I "%FB_HOME%\inc\win\rc" -i SmartMath.rc -o "%OUT_DIR%\SmartMath_res.o"
-if errorlevel 1 exit /b 1
 
 rem fbc flags (see also Compile32.bat):
 rem   -strip     Passes -s to ld: remove symbol table / relocation info from the PE (smaller file).
@@ -30,7 +24,7 @@ rem   -Wc -O2    GCC: optimize for speed.
 rem 32-bit DLL can be slightly larger than 64-bit for the same source: more stack spills / different
 rem tuning in libgcc+runtime, x87 vs SSE paths, and x64 sometimes shrinking hot code via extra GPRs.
 
-fbc -dll -gen gcc -arch x86_64 -x "%OUT_DIR%\SmartMath.dll" -strip -O 2 -Wc -O2 "SmartMath.bas" "SmartMath_Config.bas" "SmartMath_Format.bas" "SmartMath_CopyNormalize.bas" "SmartMath_About.bas" "SmartMath_Menu.bas" "MathParser.bas" "%OUT_DIR%\SmartMath_res.o"
+fbc -dll -gen gcc -arch x86_64 -x "%OUT_DIR%\SmartMath.dll" -strip -O 2 -Wc -O2 "SmartMath.bas" "SmartMath_Config.bas" "SmartMath_Format.bas" "SmartMath_CopyNormalize.bas" "SmartMath_About.bas" "SmartMath_Menu.bas" "MathParser.bas" "SmartMath.rc"
 if exist "%OUT_DIR%\*.a" del "%OUT_DIR%\*.a"
 if exist "%OUT_DIR%\*.o" del "%OUT_DIR%\*.o"
 REM pause
